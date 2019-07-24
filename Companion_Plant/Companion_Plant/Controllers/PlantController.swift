a8d8a3c95a1883fd9e86cba4883496070e61fe5d
Leah <musicalshorty65@gmail.com>
2019-07-24 15:11:31 -0600

    view files created

--- Companion_Plant/Companion_Plant/Controllers/PlantController.swift
+++ Companion_Plant/Companion_Plant/Controllers/PlantController.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+
+class PlantController {
+    
+    static let SharedInstance = PlantController()
+    
+    var plants: [Plant] = []
+}
--- Companion_Plant/Companion_Plant/Models/Plant.swift
+++ Companion_Plant/Companion_Plant/Models/Plant.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2019 Leah Cluff. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Plant {
+    
+    var plantName: String
+    var date: Date
+    var plantDescription: String
+    var plantLog: String
+    var plantImage: UIImage
+    
+    init(plantName: String, date: Date, plantDescription: String, plantLog: String, plantImage: UIImage) {
+        self.plantName = plantName
+        self.date = date
+        self.plantDescription = plantDescription
+        self.plantLog = plantLog
+        self.plantImage = plantImage
+    }
+}
+
